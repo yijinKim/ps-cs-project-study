fe4f8f295d01c5df31874ec435d1f2d8b3e48639
Yijin <yijin3018@gmail.com>
2021-11-10 01:19:16 +0900

    두번째 예제 통과 못함

---
+++ Problem Solving/01주차/매칭 점수_yijin.py
@@ -0,0 +1,39 @@
+import re
+from collections import defaultdict
+
+
+# 두번째 예제는 통과 못함.
+def solution(word, pages):
+
+    score_dict = defaultdict(int)
+
+    for page in pages:
+        url = re.search('<meta property="og:url" content="(\S+)"', page).group(1)
+        score = 0
+        # print(url)
+
+        # 기본 점수
+        for find_word in re.findall(r'[A-Za-z]+', page.lower()):
+            if find_word == word.lower():
+                score += 1
+        score_dict[url] = score
+
+        # 외부 링크 수
+        external_link = re.findall('<a href="(https://[\S]*)"', page)
+        # print(external_link)
+
+        # 링크 점수
+        link_score = score / len(external_link)
+
+        # 연결된 링크
+        for link in external_link:
+            score_dict[link] += link_score
+
+    lst = list(score_dict.values())
+
+    return lst.index(max(lst))
+
+
+word = 'Muzi'
+pages =["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]
+print(solution(word, pages))

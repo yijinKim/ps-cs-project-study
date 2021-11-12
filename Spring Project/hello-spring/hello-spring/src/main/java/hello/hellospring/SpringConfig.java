916ea3b8384cb9e7351de3061d93f695e9ab1cae
Yijin <yijin3018@gmail.com>
2021-11-12 11:46:24 +0900

    update 회원 웹기능 조회

---
+++ Spring Project/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,19 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}

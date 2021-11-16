a018565ecb494add93a08c7b58eff4488e61502d
Yijin <yijin3018@gmail.com>
2021-11-16 13:32:27 +0900

    create Week 02

--- Problem Solving/02주차/평범한 배낭2_yijin.py
+++ Problem Solving/02주차/평범한 배낭2_yijin.py
@@ -3,21 +3,25 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split()) # N 물품수, M 무게
 items = [map(int, input().split()) for _ in range(N)]  # V 무게 C 만족도 K 물건개수
-
 dp = [0 for _ in range(M+1)]
+w, s = [], []
 
 for i in range(N):
     v, c, k = items[i]
-    t = 1
+    idx = 1
     temp = 0
     while k>0:
-        temp = min(k, t)
-        for j in range(M, v*temp, -1):
-            dp[j] = max(dp[j - v*temp] + c*temp, dp[j])
-        t *= 2
+        temp = min(k, idx)
+        w.append(v*temp)
+        s.append(c*temp)
+
+        idx *= 2
         k -= temp
 
-ans = 0
-for i in range(M):
-    ans = max(dp[i], ans)
-print(ans)
+
+for i in range(len(w)):
+    for j in range(M, 0, -1):
+        if j >= w[i]:
+            dp[j] = max(dp[j] , dp[j-w[i]]+s[i])
+
+print(dp[M])

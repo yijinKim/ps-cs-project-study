161e731db0184b335abaf7f68adecb2f63f5a4e0
Yijin <yijin3018@gmail.com>
2021-12-14 17:31:38 +0900

    Update: 카카오머니

--- Problem Solving/06주차/카카오머니.py
+++ Problem Solving/06주차/카카오머니.py
@@ -4,10 +4,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-balance = 0
+prev_balance = 0
 charge_unit = 0 #최소 충전 단위
 MAXI = 10 ** 18
-min_b = MAXI
+min_balance = MAXI
 available_value = True
 
 
@@ -18,21 +18,21 @@ def gcd(x, y):
     
 
 for _ in range(n):
-    a, b = map(int, input().split())
+    money, balance = map(int, input().split())
     
-    if balance + a < 0:  # 충전이 필요한 경우
-        if b!= 0 and b < min_b:
-            min_b = b
-        charge_unit = gcd(b - balance - a, charge_unit)
+    if prev_balance + money < 0:  # 충전이 필요한 경우
+        if balance!= 0 and balance < min_balance:
+            min_balance = balance
+        charge_unit = gcd(balance - prev_balance - money, charge_unit)
         
-        if charge_unit <= min_b and min_b != MAXI:
+        if charge_unit <= min_balance and min_balance != MAXI:
             available_value = False
             break
-    else: #  balance + a >= 0  # 충전 필요 없이 계산됨.
-        if balance + a != b: # 잔액이 맞지 않는 경우
+    else: #  prev_balance + a >= 0  # 충전 필요 없이 계산됨.
+        if prev_balance + money != balance: # 잔액이 맞지 않는 경우
             available_value = False
             break
-    balance = b 
+    prev_balance = balance 
 
 
 if available_value and charge_unit != 0:
